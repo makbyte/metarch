88812855ac98ee8d99dfec1b7ad1824dbebd98e8
aftab-08khan <akkhanaftab08@gmail.com>
2025-09-16 19:34:15 +0530

    UI | Height fixed in the loading Components

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-3/4 items-center justify-center">
       <div className="flex flex-col items-center space-y-4">
         <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-600 border-t-transparent"></div>
         <p className="text-sm text-gray-700 dark:text-gray-300">
